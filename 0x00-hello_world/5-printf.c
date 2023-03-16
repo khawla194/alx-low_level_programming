6b93c4eb9504fed8452fde2a19806453f50676e3
khawla <khawlaelbardouni@gmail.com>
2023-03-16 12:30:04 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
- * Return 0 for success
- **/
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
