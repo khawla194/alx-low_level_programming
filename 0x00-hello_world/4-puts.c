19fe4e15c605ec33f396467a7f0aaf8495aa81d0
khawla <khawlaelbardouni@gmail.com>
2023-03-16 12:05:27 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /* main integer
  * function returns always 0
- * */
+ **/
 	
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
