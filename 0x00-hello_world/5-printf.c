556a7777a91340d68412ebca09ddf71c53f425f0
khawla <khawlaelbardouni@gmail.com>
2023-03-16 12:18:25 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * function main integer
+ * Return 0 for success
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
