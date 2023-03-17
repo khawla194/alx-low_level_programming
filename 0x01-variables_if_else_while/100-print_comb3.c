209e19a6c1e52ba2b0785c0174c5a9423f5e2bc6
khawla <khawlaelbardouni@gmail.com>
2023-03-17 13:40:39 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 9; j++)
 		{
-			if (j == i)
+			if (j>i)
 			{
-				continue;
-			}
-			else {
 			putchar (i + 48);
 			putchar (j + 48);
+			if (i != 8 || j != 9)
+			{
 			putchar (44);
 			putchar (32);
+			}
 			}
 		}
 	}
