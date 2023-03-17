9aeb3367f3c42f246ce84c882bc3edf8b3cc16c2
khawla <khawlaelbardouni@gmail.com>
2023-03-17 11:08:21 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,15 @@
 */
 int main(void)
 {
-	int i;
-	int alpha = 97;
+        int i;
+        int alpha = 97;
 
-	for(i = 0;i < 26; i++)
-	{
-		putchar(alpha);
-		alpha ++;
-	}
-	putchar ('\n');
+        for(i = 0;i < 26; i++)
+        {
+                putchar(alpha);
+                alpha ++;
+        }
+        putchar ('\n');
 return(0);
 }
+
