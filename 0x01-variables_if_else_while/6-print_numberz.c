6184d2f31e4b4b483f836bcb2eeca94eb2c13e74
khawla <khawlaelbardouni@gmail.com>
2023-03-17 12:38:18 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-        int i;
-
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-        }
-        putchar ('\n');
-        return (0);
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar ('\n');
+	return (0);
 }
