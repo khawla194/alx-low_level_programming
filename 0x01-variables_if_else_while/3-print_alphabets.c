f89d48e33e1e118950f8279d4ec59912624e579f
khawla <khawlaelbardouni@gmail.com>
2023-03-17 11:46:00 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int alpha = 97;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha);
+		alpha ++;
+	}
+	alpha = 65;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha);
+		alpha ++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
