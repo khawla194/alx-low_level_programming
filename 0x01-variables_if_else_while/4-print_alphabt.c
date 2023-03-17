6f85137eebcca6ad1e350c05c845f9ff99935070
khawla <khawlaelbardouni@gmail.com>
2023-03-17 12:12:41 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int alpha = 97;
+
+	for (i = 0;i < 26; i++)
+	{
+		if (alpha == 101 || alpha == 113)
+		{
+			continue;
+		}
+		else
+		{
+			putchar (alpha);
+		}
+		alpha++;
+	}
+	putchar ('\n');
+	return (0);
+}
