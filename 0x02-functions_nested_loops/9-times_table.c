a69a0c23dfd60a06630f8f53d653ddd6a14aac96
khawla <khawlaelbardouni@gmail.com>
2023-03-21 14:16:10 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
+ * times_table - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,13 +9,18 @@ void times_table(void)
 	int i;
 	int j;
 	int p;
+	int a;
+	int b;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			p = i * j;
-			_putchar(p + 48);
+			a = p / 10;
+			b = p % 10;
+			_putchar(a + 48);
+			_putchar(b+48);
 			_putchar(',');
 			_putchar('.');
 		}
