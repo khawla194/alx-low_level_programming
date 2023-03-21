fe7272b4634416d4189fff4a28583cc4022f8460
khawla <khawlaelbardouni@gmail.com>
2023-03-21 14:49:55 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @a : integer
+ *
+ * @b : integer
+ *
+ * Return: Always addition (Success)
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
