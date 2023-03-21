e96f2f4e2b222ec9e16924864d66e7f5343242d1
khawla <khawlaelbardouni@gmail.com>
2023-03-21 12:42:19 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sing - Entry point
+ *
+ * @n is integer
+ *
+ * Return: 0 1 or -1 (Success)
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		if (n > 0)
+		{
+			_putchar ('+');
+			return (1);
+		}
+		else
+		{
+			_putchar ('-');
+			return (-1);
+		}
+	}
+}
