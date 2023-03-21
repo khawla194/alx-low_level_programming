01caf5e1cac371fe7b5410c2ce9541c3f7fcc2d0
khawla <khawlaelbardouni@gmail.com>
2023-03-21 12:01:51 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(j + 97);
+		}
+		_putchar('\n');
+	}
+}
