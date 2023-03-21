cf5f4ce35610cd9853c4087dd4c82d9c4a96c570
khawla <khawlaelbardouni@gmail.com>
2023-03-21 13:03:19 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @c : integer
+ *
+ * Return: a value (Success)
+ */
+int print_last_digit(int c)
+{
+int a;
+
+a = c % 10;
+return (a);
+}
