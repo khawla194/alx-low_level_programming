b8ee1214e72c69b9721975e287152d47e1ad01a0
khawla <khawlaelbardouni@gmail.com>
2023-03-23 11:27:21 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - print line
+ *
+ * @n : integer
+ *
+ * Return: none
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
