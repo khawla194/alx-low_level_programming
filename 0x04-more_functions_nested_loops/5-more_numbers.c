8293291de2ab07ad156ab6e1ee1bd9c4322b3711
khawla <khawlaelbardouni@gmail.com>
2023-03-23 10:52:11 -0700

    print number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,22 +8,17 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + 48);
-			if (j == 9)
+			if (j >= 10)
 			{
-				for (k = 0; k < 5; k++)
-				{
 				_putchar('1');
-				_putchar(k + 48);
-				}
 			}
-		}
+				_putchar((j % 10) + 48);
+			}
 		_putchar('\n');
 	}
 }
