b263b8044cc808dc1624acae4ed935f883da9658
khawla <khawlaelbardouni@gmail.com>
2023-03-27 14:07:06 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print string
+ *
+ * @str : string
+ *
+ * Return: None
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
