4d7f7e8d2c34e3e7263211d4f4edb9e71f5424ce
khawla <khawlaelbardouni@gmail.com>
2023-03-27 14:23:31 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @s : string
+ *
+ * Return: None
+ */
+void print_rev(char *s)
+{
+	int l;
+	int i;
+
+	l = 0;
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+	for (i = 0; i <= l; i++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
