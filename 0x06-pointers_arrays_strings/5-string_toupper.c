49588d3fcabd9b02a79aa235e2eee72787f0a27b
khawla <khawlaelbardouni@gmail.com>
2023-03-29 13:39:45 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *string_toupper -  changes all lowercase letters of a string to uppercase
+ *
+ * @s : string
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
