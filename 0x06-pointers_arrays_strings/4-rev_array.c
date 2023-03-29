8b0dda17249d9441e53a2f9dd46f27a6e3fb244f
khawla <khawlaelbardouni@gmail.com>
2023-03-29 13:32:45 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
+		n--;
+	}
+}
