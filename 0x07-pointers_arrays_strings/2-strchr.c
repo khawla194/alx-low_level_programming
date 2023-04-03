bfba659274af68bafc7cf9c63604016eb1ae7808
khawla <khawlaelbardouni@gmail.com>
2023-04-03 14:59:31 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strchr - a function that locates a character in a string
+ *
+ * @s : char
+ *
+ * @c : char
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+	}
+	if (s[i] == c)
+	{
+	return (&s[i]);
+	}
+	else
+	{
+		return (0);
+	}
+}
