4da9e3ad4d675fad72413cb774cc08e2f779b988
khawla <khawlaelbardouni@gmail.com>
2023-04-05 10:52:30 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion -  prints a string in reverse.
+ *
+ * @s : string
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
