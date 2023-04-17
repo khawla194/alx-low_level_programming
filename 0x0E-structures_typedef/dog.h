a642e832cafdb873976a140f97f5d473a7c8d8e7
khawla <khawlaelbardouni@gmail.com>
2023-04-17 11:33:34 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type struct dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
