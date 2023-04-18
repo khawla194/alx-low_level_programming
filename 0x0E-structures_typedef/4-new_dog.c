1734a1999f066ecd1798f0e752a79499b8f2ef2c
khawla <khawlaelbardouni@gmail.com>
2023-04-18 12:54:35 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
 	d->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (d->name == NULL)
 	{
