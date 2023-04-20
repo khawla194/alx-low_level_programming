03c93216e83d2dc61e4906f5910f0b2ee17f29dd
khawla <khawlaelbardouni@gmail.com>
2023-04-20 14:12:54 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	char * str;
+	char *str;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
